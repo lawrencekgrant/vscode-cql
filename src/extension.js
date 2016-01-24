c6e5b1015a2d097d1d4f2d96eceaa4b834fa3b5e
Lawrence Grant <karbon@Lawrences-MacBook-Pro.local>
2016-01-24 16:35:00 -0700

    Removing useless stuff.

--- src/extension.js
+++ src/extension.js
@@ -5,28 +5,7 @@ var vscode = require('vscode');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cql" is now active!'); 
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('CQL baby!');
-	});
-    
-    vscode.languages.registerHoverProvider('cql', function() {
-        vscode.provideHover(document, position, token) {
-            return new Hover('I am a hover!');
-        }
-    });
-	
-	context.subscriptions.push(disposable);
+    console.log('Nothing is happening here guys...');
 }
 exports.activate = activate;
 
