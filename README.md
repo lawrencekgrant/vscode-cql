66ebe287b670f23edb3be7aeb678e99a0198fd33
Lawrence Grant <karbon@Lawrences-MacBook-Pro.local>
2016-01-24 16:31:35 -0700

    Adding angle brackets. More README changes. Addition of extension.js that does nothing.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # CQL support for VS Code
 
 ## This is a language extension for the Cassandra CQL language.
-Currently this addon only adds syntax highlighting for .cql files. This just makes it a bit easier to use the .cql extension without resorting to mapping .sql to .cql. It allows you to keep the two separate.
+Currently this addon only adds syntax highlighting for .cql files. This just makes it a bit easier to use the .cql extension without resorting to mapping .sql to .cql. 
+
+Currently Supports:
+ - Syntax Highlighting
+ - Brackets
+ - Comments
 
 ### For more information
 * [github](https://github.com/lawrencekgrant/vscode-cql)
---
+++ src/extension.js
@@ -0,0 +1,36 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+var vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "cql" is now active!'); 
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('CQL baby!');
+	});
+    
+    vscode.languages.registerHoverProvider('cql', function() {
+        vscode.provideHover(document, position, token) {
+            return new Hover('I am a hover!');
+        }
+    });
+	
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
