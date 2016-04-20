587e96e3f5e91b585afef6deb526b7660c994d4e
Lawrence Grant <karbon@Lawrences-MacBook-Pro.local>
2016-04-19 17:45:48 -0700

    Switching machines. Commit everything.

--- out/src/extension.js
+++ out/src/extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
+var cassandra = require('cassandra-driver');
 var keywords = ["ADD", "ALL", "ALLOW", "ALTER", "AND", "ANY", "APPLY", "ASC", "ASCII", "AUTHORIZE", "BATCH", "BEGIN", "BIGINT", "BLOB", "BOOLEAN", "BY", "CLUSTERING", "COLUMNFAMILY", "COMPACT", "COUNT", "COUNTER", "CONSISTENCY", "CREATE", "DECIMAL", "DELETE", "DESC", "DOUBLE", "DROP", "EACH_QUORUM", "FILTERING", "FLOAT", "FROM", "GRANT", "IN", "INDEX", "INET", "INSERT", "INT", "INTO", "KEY", "KEYSPACE", "KEYSPACES", "LEVEL", "LIMIT", "LIST", "LOCAL_ONE", "LOCAL_QUORUM", "MAP", "MODIFY", "NORECURSIVE", "NOSUPERUSER", "OF", "ON", "ONE", "ORDER", "PASSWORD", "PERMISSION", "PERMISSIONS", "PRIMARY", "QUORUM", "RENAME", "REVOKE", "SCHEMA", "SELECT", "SET", "STORAGE", "SUPERUSER", "TABLE", "TEXT", "TIMESTAMP", "TIMEUUID", "TO", "TOKEN", "THREE", "TRUNCATE", "TTL", "TWO", "TYPE", "UNLOGGED", "UPDATE", "USE", "USER", "USERS", "USING", "UUID", "VALUES", "VARCHAR", "VARINT", "WHERE",
     "WITH", "WRITETIME"];
 var allCompletionItems = [];
@@ -12,6 +13,39 @@ function activate(context) {
         vscode.window.showInformationMessage('This is an extension to help me with my projects!!');
     });
     context.subscriptions.push(disposable);
+    var executeCommandDisposable = vscode.commands.registerCommand('cql.execute', function () {
+        var cassandraLocation = vscode.workspace.getConfiguration("cql.configuration");
+        console.log('cl', cassandraLocation);
+        vscode.window.showInformationMessage("Executing statement against Cassandra @ " + cassandraLocation['cassandra.location']);
+        var conf = vscode.workspace.getConfiguration('cassandra.address');
+        console.log(conf);
+        console.log('cassandra location', cassandraLocation);
+        var client = new cassandra.Client({
+            contactPoints: ['127.0.0.1'],
+            hosts: ['127.0.0.1']
+        });
+        vscode.workspace.getConfiguration("cassandra.cql");
+        var statement = "";
+        if (vscode.window.activeTextEditor.selection.isEmpty) {
+            statement = vscode.window.activeTextEditor.document.getText();
+        }
+        else {
+            var selection = vscode.window.activeTextEditor.selection;
+            statement = vscode.window.activeTextEditor.document.getText(new vscode.Range(selection.start, selection.end));
+        }
+        console.log("statement: " + statement);
+        client.connect(function (err, result) {
+            client.execute(statement.toString(), [], { prepare: true }, function (err, result) {
+                console.log('executed', err, result);
+                var outputChannel = vscode.window.createOutputChannel("CQL Results");
+                var resultText = "";
+                outputChannel.clear();
+                outputChannel.append(JSON.stringify(err ? err : result));
+                outputChannel.show();
+            });
+        });
+    });
+    context.subscriptions.push(executeCommandDisposable);
     var subs = vscode.workspace.onDidOpenTextDocument(function (evt) {
         var source = "";
         for (var i = 0; i < evt.lineCount; i++) {
@@ -46,6 +80,20 @@ function activate(context) {
         }
     });
     context.subscriptions.push(subs);
+    var disposable4 = vscode.languages.registerCodeActionsProvider("cql", {
+        provideCodeActions: function (document, range, context, token) {
+            var returnCommands = [];
+            vscode.commands.getCommands(true).then(function (cmd) {
+                cmd.forEach(function (val) {
+                    if (val.toLowerCase().startsWith("cql"))
+                        returnCommands.push(val);
+                });
+            });
+            console.log("providing");
+            return returnCommands;
+        }
+    });
+    context.subscriptions.push(disposable4);
     var disposable2 = vscode.languages.registerCompletionItemProvider("cql", {
         resolveCompletionItem: function (item, token) {
             return item;
--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,77 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'; 
-var keywords = ["ADD","ALL","ALLOW","ALTER","AND","ANY","APPLY","ASC","ASCII","AUTHORIZE","BATCH","BEGIN","BIGINT","BLOB","BOOLEAN","BY","CLUSTERING","COLUMNFAMILY","COMPACT","COUNT","COUNTER","CONSISTENCY","CREATE","DECIMAL","DELETE","DESC","DOUBLE","DROP","EACH_QUORUM","FILTERING","FLOAT","FROM","GRANT","IN","INDEX","INET","INSERT","INT","INTO","KEY","KEYSPACE","KEYSPACES","LEVEL","LIMIT","LIST","LOCAL_ONE","LOCAL_QUORUM","MAP","MODIFY","NORECURSIVE","NOSUPERUSER","OF","ON","ONE","ORDER","PASSWORD","PERMISSION","PERMISSIONS","PRIMARY","QUORUM","RENAME","REVOKE","SCHEMA","SELECT","SET","STORAGE","SUPERUSER","TABLE","TEXT","TIMESTAMP","TIMEUUID","TO","TOKEN","THREE","TRUNCATE","TTL","TWO","TYPE","UNLOGGED","UPDATE","USE","USER","USERS","USING","UUID","VALUES","VARCHAR","VARINT","WHERE"
-,"WITH","WRITETIME"];
+import * as vscode from 'vscode';
+import * as cassandra from 'cassandra-driver';
 
-var allCompletionItems:vscode.CompletionItem[] = [];
+var keywords = ["ADD", "ALL", "ALLOW", "ALTER", "AND", "ANY", "APPLY", "ASC", "ASCII", "AUTHORIZE", "BATCH", "BEGIN", "BIGINT", "BLOB", "BOOLEAN", "BY", "CLUSTERING", "COLUMNFAMILY", "COMPACT", "COUNT", "COUNTER", "CONSISTENCY", "CREATE", "DECIMAL", "DELETE", "DESC", "DOUBLE", "DROP", "EACH_QUORUM", "FILTERING", "FLOAT", "FROM", "GRANT", "IN", "INDEX", "INET", "INSERT", "INT", "INTO", "KEY", "KEYSPACE", "KEYSPACES", "LEVEL", "LIMIT", "LIST", "LOCAL_ONE", "LOCAL_QUORUM", "MAP", "MODIFY", "NORECURSIVE", "NOSUPERUSER", "OF", "ON", "ONE", "ORDER", "PASSWORD", "PERMISSION", "PERMISSIONS", "PRIMARY", "QUORUM", "RENAME", "REVOKE", "SCHEMA", "SELECT", "SET", "STORAGE", "SUPERUSER", "TABLE", "TEXT", "TIMESTAMP", "TIMEUUID", "TO", "TOKEN", "THREE", "TRUNCATE", "TTL", "TWO", "TYPE", "UNLOGGED", "UPDATE", "USE", "USER", "USERS", "USING", "UUID", "VALUES", "VARCHAR", "VARINT", "WHERE"
+    , "WITH", "WRITETIME"];
+
+var allCompletionItems: vscode.CompletionItem[] = [];
 var tablesNames = [];
 var columnNames = [];
 var completionItemNames = []
 
 export function activate(context: vscode.ExtensionContext) {
-    
+
     var disposable = vscode.commands.registerCommand('cql.about', () => {
-		vscode.window.showInformationMessage('This is an extension to help me with my projects!!');
-	});
-    
+        vscode.window.showInformationMessage('This is an extension to help me with my projects!!');
+    });
     context.subscriptions.push(disposable);
-    
+
+    var executeCommandDisposable = vscode.commands.registerCommand('cql.execute', () => {
+        var cassandraLocation = vscode.workspace.getConfiguration("cql.configuration");
+        console.log('cl', cassandraLocation);
+
+        vscode.window.showInformationMessage(`Executing statement against Cassandra @ ${cassandraLocation['cassandra.location']}`);
+        let conf = vscode.workspace.getConfiguration('cassandra');
+        
+        console.log(conf);
+        console.log('cassandra location', cassandraLocation);
+        var cassandraAddress = conf.get('address');
+        var cassandraPort = conf.get('port');
+
+        var client = new cassandra.Client({
+            contactPoints: [cassandraAddress],
+            hosts: [cassandraAddress]
+        });
+
+        vscode.workspace.getConfiguration("cassandra.cql")
+
+        var statement = "";
+        if (vscode.window.activeTextEditor.selection.isEmpty) {
+            statement = vscode.window.activeTextEditor.document.getText();
+        } else {
+            var selection = vscode.window.activeTextEditor.selection;
+            statement = vscode.window.activeTextEditor.document.getText(new vscode.Range(selection.start, selection.end));
+        }
+
+        console.log("statement: " + statement);
+
+        client.connect(function(err, result) {
+            client.execute(statement.toString(), [], { prepare: true }, function(err, result) {
+                console.log('executed', err, result);
+
+                var outputChannel = vscode.window.createOutputChannel("CQL Results");
+                var resultText = "";
+
+                outputChannel.clear();
+                outputChannel.append(JSON.stringify(err ? err : result));
+                outputChannel.show();
+            });
+        });
+    });
+    context.subscriptions.push(executeCommandDisposable);
+
     var subs = vscode.workspace.onDidOpenTextDocument(function(evt) {
         var source = "";
-        for(var i = 0; i < evt.lineCount; i++) {
+        for (var i = 0; i < evt.lineCount; i++) {
             source += evt.lineAt(i).text;
         }
-        var re = /(?:create table)\s(.*)\(/i; 
+        var re = /(?:create table)\s(.*)\(/i;
         var tableName = source.match(/(?:create table)\s(.*)\(/i)[1];
         console.log(source.match(/(?:create table)\s(.*)\(/i));
-        
-        if(tableName) {
+
+        if (tableName) {
             var item = new vscode.CompletionItem(tableName);
             item.detail = "Table: " + tableName;
             item.filterText = tableName;
@@ -34,19 +79,18 @@ export function activate(context: vscode.ExtensionContext) {
             item.kind = vscode.CompletionItemKind.Class;
             allCompletionItems.push(item);
         }
-        
-        var columns= source.match(/.*CREATE\s+TABLE\s+(\S+)\s*\((.*)\).*/i)[2].split(/,/);
-        
-        for(i = 0;i < columns.length; i++) {
+
+        var columns = source.match(/.*CREATE\s+TABLE\s+(\S+)\s*\((.*)\).*/i)[2].split(/,/);
+
+        for (i = 0; i < columns.length; i++) {
             var val = columns[i];
             var val2 = columns[i].match(/([^\s]+)/i)[0].trim();
             columns[i] = val2;
         }
-        
-        for(var i = 0; i < columns.length; i++)
-        {
+
+        for (var i = 0; i < columns.length; i++) {
             var currentValue = columns[i];
-            
+
             var item = new vscode.CompletionItem(currentValue);
             item.detail = "Column: " + currentValue;
             item.filterText = currentValue;
@@ -56,26 +100,39 @@ export function activate(context: vscode.ExtensionContext) {
             allCompletionItems.push(item);
         }
     });
-    
+
     context.subscriptions.push(subs);
-    	
-    var disposable2 = vscode.languages.registerCompletionItemProvider("cql",
-    {
-        resolveCompletionItem(item:vscode.CompletionItem, token: vscode.CancellationToken): vscode.CompletionItem 
-        {
-            return item;
-        }
-        ,
-        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Promise<vscode.CompletionItem[]> 
-        {
-            return new Promise((resolve, reject) => { 
-                return resolve(allCompletionItems) ;
+
+    var disposable4 = vscode.languages.registerCodeActionsProvider("cql", {
+        provideCodeActions(document: vscode.TextDocument, range: vscode.Range, context: vscode.CodeActionContext, token: vscode.CancellationToken): vscode.Command[] {
+            var returnCommands = [];
+            vscode.commands.getCommands(true).then((cmd) => {
+                cmd.forEach((val) => {
+                    if (val.toLowerCase().startsWith("cql"))
+                        returnCommands.push(val);
+                });
             });
+            console.log("providing");
+            return returnCommands;
         }
-    })
-    
-	context.subscriptions.push(disposable2);
-    
+    });
+    context.subscriptions.push(disposable4);
+
+    var disposable2 = vscode.languages.registerCompletionItemProvider("cql",
+        {
+            resolveCompletionItem(item: vscode.CompletionItem, token: vscode.CancellationToken): vscode.CompletionItem {
+                return item;
+            }
+            ,
+            provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Promise<vscode.CompletionItem[]> {
+                return new Promise((resolve, reject) => {
+                    return resolve(allCompletionItems);
+                });
+            }
+        })
+
+    context.subscriptions.push(disposable2);
+
     var items = keywords.map(function(val) {
         var itm = new vscode.CompletionItem(val);
         itm.detail = val;
@@ -84,8 +141,7 @@ export function activate(context: vscode.ExtensionContext) {
         itm.kind = vscode.CompletionItemKind.Keyword;
         return itm;
     });
-    for(var i = 0; i < items.length; i++)
-    {
+    for (var i = 0; i < items.length; i++) {
         allCompletionItems.push(items[i]);
     }
 }
