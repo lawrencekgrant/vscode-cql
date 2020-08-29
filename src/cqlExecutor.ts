27515e243a7d45a65d728f924ad8331cadd98b0c
Alex Leventer <alexleventer@gmail.com>
2020-08-28 21:55:33 -0700

    statement -> query

--- src/cqlExecutor.ts
+++ src/cqlExecutor.ts
@@ -8,22 +8,21 @@ export let currentResults = {};
 let outputChannel = vscode.window.createOutputChannel(`CQL Output`);
 
 export function registerExecuteCommand() : vscode.Disposable {
-    return vscode.commands.registerCommand('cql.execute', ()=> {
-        var statement = "";
+    return vscode.commands.registerCommand('cql.execute', () => {
+        let query;
         if (vscode.window.activeTextEditor.selection.isEmpty) {
-            statement = vscode.window.activeTextEditor.document.getText();
+            query = vscode.window.activeTextEditor.document.getText();
         }
         else {
             var selection = vscode.window.activeTextEditor.selection;
-            statement = vscode.window.activeTextEditor.document.getText(new vscode.Range(selection.start, selection.end));
+            query = vscode.window.activeTextEditor.document.getText(new vscode.Range(selection.start, selection.end));
         }
-        executeCqlStatement(statement);
-        
+        executeCqlQuery(query);
     });
 }
 
-export function executeCqlStatement(statement: string) {
-    console.log('Configuring cql statement execution.');
+export async function executeCqlQuery(query: string) {
+    console.log('Configuring cql query execution.');
     const cassandraAddress = vscode.workspace.getConfiguration("cql")["address"];
     const cassandraPort = vscode.workspace.getConfiguration("cql")["port"];
     const cassandraConnectionOptions = vscode.workspace.getConfiguration("cql")["connection"];
@@ -31,7 +30,7 @@ export function executeCqlStatement(statement: string) {
     const cloudPassword = vscode.workspace.getConfiguration("cql")["password"];
     const secureConnectBundle = vscode.workspace.getConfiguration("cql")["secureConnectBundle"];
 
-    let clientOptions:any;
+    let clientOptions;
     if (cloudUsername && cloudPassword && secureConnectBundle) {
         clientOptions = {
             cloud: { secureConnectBundle: secureConnectBundle},
@@ -48,25 +47,20 @@ export function executeCqlStatement(statement: string) {
     console.log('Cassandra connection configuration', clientOptions);
     
     const client = new cassandra.Client(clientOptions);
-    var statement = statement;
-    console.log("statement: " + statement);
+    console.log("query: " + query);
     outputChannel.show();
-    outputChannel.appendLine(`Executing statement: "${statement}"`);
+    outputChannel.appendLine(`Executing query: "${query}"`);
 
-    client.connect((err, result) => {
-        client.execute(statement.toString(), [], { prepare: true }, function (err, result) {
-            console.log('executed', err, result);
-            if (err) {
-                currentResults = err;
-                outputChannel.appendLine(`Error executing statement: ${util.inspect(err)}`)
-            } else {
-                currentResults = result;
-                outputChannel.appendLine(`Execution successful.`)
-            }
-
-            showResults(err, result);
+    client.connect()
+        .then(() => {
+            return client.execute(query.toString(), [], {prepare: true})
+        })
+        .then((results) => {
+            outputChannel.appendLine(JSON.stringify(results, null, 2));
+        })
+        .catch(err => {
+            outputChannel.appendLine(`Error executing query: ${util.inspect(err)}`)
         });
-    });
 }
 
 export function registerResultDocumentProvider() : vscode.Disposable {
@@ -76,21 +70,4 @@ export function registerResultDocumentProvider() : vscode.Disposable {
 
 export function registerAll() : vscode.Disposable[] { //I like that.. I may keep this.
     return [registerExecuteCommand(), registerResultDocumentProvider()];
-}
-
-function showResults(error, results) {
-    if(vscode.workspace.getConfiguration("cql")["resultStyle"].location == "output") {
-        outputChannel.appendLine("Results:");
-        outputChannel.appendLine(util.inspect(error ? error : currentResults, {depth: 64}));
-        outputChannel.appendLine(new Date().toTimeString());
-        outputChannel.show();
-    } else {
-        let resultUri = `cql-result://api/results${uuid.v4()}?error=${!!error}`;
-        vscode.commands.executeCommand('vscode.previewHtml', resultUri, vscode.ViewColumn.Two, 'Cassandra Execution Results')
-            .then((success) => {
-                //do nothing it worked already...
-            }, (reason) => {
-                vscode.window.showErrorMessage(reason);
-            });
-    }
 }
